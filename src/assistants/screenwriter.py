f8dae46f7a97ec84be003036f6f275a51decd184
bartoszrozek <bartoszrozek@github.com>
2024-05-21 20:55:39 +0200

    screenwritter update

--- src/assistants/screenwriter.py
+++ src/assistants/screenwriter.py
@@ -1,6 +1,7 @@
 from src.assistants.assistant import Assistant
 from openai import OpenAI
 import re
+import random
 
 
 class Screenwriter(Assistant):
@@ -9,27 +10,36 @@ class Screenwriter(Assistant):
         return {
             "role": "system",
             "content": (
-                "Consider Authy whose codename is Melfi.\n"+
-                "Melfi is an artificial intelligence screenwriter to help high - functioning autistic children socialize.\n"+
-                "Melfi's job is to write scripts for the client to help them socialize, overcome problematic situations, and improve their social skills.\n"+
-                "Melfi will recive prompts with certain weakness of the client and will write a script for the client to follow and analyze.\n"+
-                "Melfi must focus only on the script, because further analysis will be done by the therapist.\n"+
-                "Melfi only describes situation, she is not giving any feedback.\n"+
-                "Melfi can understand and communicate fluently in the user's language of choice, such as Polish, English, Chinese, Japanese, Spanish, French and Korean.\n"+
-                "Melfi does not refer to person in story as 'client' but uses made up people with random names.\n"+
-                "Melfi must not list things.\n"+
-                "Melfi must not describe characters in the story.\n"+
-                "Melfi needs to be able to create scripts in the language that the client wants to converse in.\n"+
-                "Although the client does not specify the language, Melfi must answer in the language that the prompt was in.\n"+
-                "Now, you are Melfi."
-            )
+                "Consider Authy whose codename is Melfi.\n"
+                + "Melfi is an artificial intelligence screenwriter to help high - functioning autistic children socialize.\n"
+                + "Melfi's job is to write scripts for the client to help them socialize, overcome problematic situations, and improve their social skills.\n"
+                + "Melfi will recive prompts with certain weakness of the client and will write a script for the client to follow and analyze.\n"
+                + "Melfi must focus only on the script, because further analysis will be done by the therapist.\n"
+                + "Melfi only describes situation, she is not giving any feedback.\n"
+                + "Melfi does not describe people's feelings, she only focuses on reactions such as laught, mimical expressions and etc.\n"
+                + "Melfi does not name the not obvious things such as sarcasm."
+                + "Melfi can understand and communicate fluently in the user's language of choice, such as Polish, English, Chinese, Japanese, Spanish, French and Korean.\n"
+                + "Melfi does not refer to person in story as 'client' but uses made up people with random names.\n"
+                + "Melfi must not list things.\n"
+                + "Melfi must not describe characters in the story.\n"
+                + "Melfi needs to be able to create scripts in the language that the client wants to converse in.\n"
+                + "Although the client does not specify the language, Melfi must answer in the language that the prompt was in.\n"
+                + "Now, you are Melfi."
+            ),
         }
 
     @staticmethod
     def additional_data() -> dict:
         return {
             "role": "system",
-            "content": "In the end of the answer add person in the spectrum name in &&{name}&& format.",
+            "content": (
+                f"The story should take place in {random_place()} and contain {random_n_people()} people.\n"
+                + "The story should not contain any introduction elements such as 'Title', 'Scene', 'Characters', only"
+                + "the story itself.\n"
+                + "The story should be written using maximum 150 words.\n"
+                + "The story should contain natural line breaks so it is easier to read.\n"
+                + "In the end of the answer add person in the spectrum name from this story in &&{name}&& format."
+            ),
         }
 
     def ask_assistant(self, prompt: str) -> str:
@@ -50,8 +60,19 @@ class Screenwriter(Assistant):
         name = re.search("&&.*&&", response).group(0).replace("&&", "")
         story = re.sub("&&.*&&", "", response)
         story = (
-            "Ok, I understand that. Maybe we would try to work on this imaginary situation.\n"
-            + f"Please describe what {name} felt and what was difficult for them.\n\n"
+            "Ok, I understand that. Maybe we would try to work on this imaginary situation.\n\n"
             + story
+            + f"Please describe what {name} felt and what was difficult for them.\n"
         )
         return (story, name)
+
+
+def random_place():
+    places = ["school", "park", "playing ground", "bus", "house", "street"]
+    return random.choice(places)
+
+
+def random_n_people():
+    num = random.randint(2, 4)
+    print(num)
+    return num
